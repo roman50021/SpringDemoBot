976f59de0bbe0d4bf1063e501e34aaf0b5e9d72e
“Roman <fedkoroma2@gmail.comgit config --global user.email fedkoroma2@gmail.com>
2023-06-27 21:36:39 +0300

    0.1

---
+++ src/main/java/com/example/springdemobot/SpringDemoBotApplication.java
@@ -0,0 +1,12 @@
+package com.example.springdemobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoBotApplication.class, args);
+    }
+
+}
